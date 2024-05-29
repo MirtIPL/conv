72db0d4456f860c3f376c9d2e67f956990271392
Mirtl <danu.qaam@gmail.com>
2024-05-29 12:38:05 +0300

    sgp-stagen

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -28,40 +28,38 @@ class TestFooter:
     #     # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
     #     time.sleep(5)
     #
-    # @pytest.mark.test_login
-    # @allure.description('Авторизация с невалидным паролем')
-    # def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
-    #     pages.index_page.open_index_page(page)
-    #     time.sleep(5)
+    @pytest.mark.test_login
+    @allure.description('Авторизация с невалидным паролем')
+    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+        pages.index_page.open_index_page(page)
+        time.sleep(5)
+
+        pages.index_page.get_login_button(page)
+        time.sleep(5)
+        pages.index_page.get_login_fail_auth(page)
+
+        # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
+        time.sleep(5)
     #
-    #     pages.index_page.get_login_button(page)
-    #     time.sleep(5)
-    #     pages.index_page.get_login(page)
-    #
-    #     # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
-    #     time.sleep(5)
-    #
-    # @pytest.mark.test_login
-    # @allure.description('Авторизация с невалидной почтой')
-    # def test_user_should_be_able_to_open_popup_select_subscription_plan2(self, page):
-    #     pages.index_page.open_index_page(page)
-    #     time.sleep(5)
-    #
-    #     pages.index_page.get_login_button(page)
-    #     time.sleep(5)
-    #     pages.index_page.get_login(page)
-    #
-    #     # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
-    #     time.sleep(5)
+    @pytest.mark.test_login
+    @allure.description('Авторизация с невалидной почтой')
+    def test_user_should_be_able_to_open_popup_select_subscription_plan2(self, page):
+        pages.index_page.open_index_page(page)
+        time.sleep(5)
+
+        pages.index_page.get_login_button(page)
+        time.sleep(5)
+        pages.index_page.get_login_fail(page)
+        # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
+        time.sleep(5)
 
     @pytest.mark.test_login
     @allure.description('Востановление пароля')
+    @allure.step('Востановление пароля')
     def test_user_should_be_able_to_open_popup_select_subscription_plan1(self, page):
         pages.index_page.open_index_page(page)
         pages.index_page.get_login_button(page)
-        pages.index_page.get_login(page)
-        #pages.index_page.make_screenshot("тест")
-        pages.index_page.make_screenshot("example_screenshot.png")
+        pages.index_page.get_login_pass(page)
+        pages.index_page.make_screenshot(page, 'Снимок')
 
-        # assert actual_result == 'Поиск в Google', 'Google Search button text is not correct'
         time.sleep(5)
